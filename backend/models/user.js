9995893bedc869ec0f73acddfb805e3ff5f1bba7
Danny <danny.f.gomez@gmail.com>
2024-12-08 13:43:00 -0500

    update user model to include first and last name

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,6 +8,16 @@ const userSchema = new Schema({
     required: true,
     trim: true,
   },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  }, 
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
   email: {
     type: String,
     unique: true,
