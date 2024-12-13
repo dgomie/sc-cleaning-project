ebb5d473e5afd8d0441adbb213b9bda1ac257fa6
mikef13x <mikef132044@gmail.com>
2024-12-12 22:43:38 -0500

    add setup for main and app, add homepage and errorpage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { ApolloProvider } from '@apollo/client';
+// import client from './utils/client';
+import HomePage from './pages/homepage.jsx';
+import ErrorPage from './pages/errorpage.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+import App from './app';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+
+
+
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <ApolloProvider client={client}> */}
+    <RouterProvider router={router} />
+    {/* </ApolloProvider> */}
+  </React.StrictMode>,
+);
---
+++ frontend/src/pages/errorpage.jsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+    return (
+        <>
+            <h1>404 page not found</h1>
+        </>
+    )
+}
