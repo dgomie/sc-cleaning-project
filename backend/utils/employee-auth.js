dddf32ea9161d18b6cba4cd2f84529f74831eb87
Danny <danny.f.gomez@gmail.com>
2024-12-13 23:02:02 -0500

    create employee specific middleware

---
+++ backend/utils/employee-auth.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { Employee } = require('../models');
+
+const secret = process.env.JWT_SECRET;
+const expiration = '48h';
+
+module.exports = {
+  signToken: ({ id, employeeId, role }) => {
+    const payload = { id, employeeId, role };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+  authMiddleware: async ({ req }) => {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+    if (!token) {
+      return req;
+    }
+    try {
+      const { data } = jwt.verify(token, secret);
+      req.employee = data;
+      const employee = await Employee.findById(data.id);
+      if (employee) {
+        req.employee = employee;
+      }
+    } catch (err) {
+      console.error('Invalid token', err);
+    }
+    return req;
+  },
+  hashPassword: async (password) => {
+    return await bcrypt.hash(password, 10);
+  },
+  checkPassword: async (inputPassword, savedPassword) => {
+    return await bcrypt.compare(inputPassword, savedPassword);
+  }
+};
