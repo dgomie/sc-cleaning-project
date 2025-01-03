90061acd7c1e69d9638d04bef017169b3f421158
Danny <danny.f.gomez@gmail.com>
2025-01-03 01:10:32 -0500

    update config to import dotenv

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-require('dotenv').config();
+import 'dotenv/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
