d94678774f7a9fc28d2be9bbec7a2e488d2e216f
Danny <danny.f.gomez@gmail.com>
2025-01-04 21:02:22 -0500

    remove user asscociated properties from package model

--- backend/models/package.js
+++ backend/models/package.js
@@ -4,33 +4,17 @@ const packageSchema = new Schema({
   package: {
     type: String,
     required: true,
-    enum: ['standard', 'deep-cleaning', "move-in/out"],
   },
   price: {
     type: Number,
     required: true,
   },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  recurring: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
   dateCreated: {
     type: Date,
     required: true,
-    default: Date.now
+    default: Date.now,
   },
-  scheduledDate: {
-    type: Date,
-    required: true
-  }
 });
-
-
 
 const Package = model('Package', packageSchema);
 
