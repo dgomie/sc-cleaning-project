59e4c41b6c9a5f70596863542378548e534f1050
Michael Freeman <mikef132044@gmail.com>
2025-01-11 17:06:19 -0500

    change packages to be reused, add proper css on them

--- frontend/src/components/home/packages.jsx
+++ frontend/src/components/home/packages.jsx
@@ -1,49 +1,56 @@
-import React from 'react'
-import { Grid, Paper, Box, Typography, Button, useMediaQuery } from '@mui/material'
+import React from 'react';
+import { Box, Grid, Paper, Typography, Button ,useMediaQuery} from '@mui/material';
 
-export default function Packages() {
+const packages = [
+  { title: 'Package 1 Title', info: 'This is some information about Package 1. It includes details and features.', price: '$100' },
+  { title: 'Package 2 Title', info: 'This is some information about Package 2. It includes details and features.', price: '$200' },
+  { title: 'Package 3 Title', info: 'This is some information about Package 3. It includes details and features.', price: '$300' },
+];
+
+function Packages() {
     const isSmallScreen = useMediaQuery('(max-width:900px)')
-
-    return (
-        <Box
-            sx={{
-                height: isSmallScreen ? 'auto' : '100vh',
+  return (
+    
+    <Box
+      sx={{
+        padding: isSmallScreen ? 2 : 0,
+        marginTop: isSmallScreen ? '80px' : '50px'
+      }}
+    >
+      <Grid container spacing={isSmallScreen ? 2 : 4} justifyContent="center">
+        {packages.map((pkg, index) => (
+          <Grid item key={index} xs={12} sm={6} md={4} lg={3} sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Paper
+              sx={{
+                width: isSmallScreen ? 300 : '100%', // Adjust width based on screen size
+                height: isSmallScreen ? 500 : '60vh', // Adjust height based on screen size
                 display: 'flex',
+                flexDirection: 'column',
                 alignItems: 'center',
-                justifyContent: 'center',
-                padding: isSmallScreen ? 2 : 0,
-                marginTop: isSmallScreen ? '80px' : '50px'
-            }}
-        >
-            <Grid container spacing={isSmallScreen ? 2 : 4} justifyContent="center">
-                {[1, 2, 3].map((item) => (
-                    <Grid item key={item} xs={12} sm={6} md={4} lg={3} sx={{ display: 'flex', justifyContent: 'center' }}>
-                        <Paper
-                            sx={{
-                                width: isSmallScreen ? '100%' : 300, // Adjust width based on screen size
-                                height: isSmallScreen ? 'auto' : 500, // Adjust height based on screen size
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                                justifyContent: 'space-between',
-                                textAlign: 'center',
-                                padding: isSmallScreen ? 2 : 5,
-
-                            }}
-                        >
-                            <Typography variant="h5" component="div" sx={{ marginTop: 2 }}>
-                                Package {item} Title
-                            </Typography>
-                            <Typography variant="body1" component="div" sx={{ margin: 2 }}>
-                                This is some information about Package {item}. It includes details and features.
-                            </Typography>
-                            <Button variant="contained" color="primary" sx={{ marginBottom: 2 }}>
-                                Learn More
-                            </Button>
-                        </Paper>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    )
+                justifyContent: 'space-between',
+                textAlign: 'center',
+                padding: isSmallScreen ? 2 : 5,
+                marginTop: isSmallScreen ? '0px': '100px'
+              }}
+            >
+              <Typography variant="h5" component="div" sx={{ marginTop: 2 }}>
+                {pkg.title}
+              </Typography>
+              <Typography variant="body1" component="div" sx={{ margin: 2 }}>
+                {pkg.info}
+              </Typography>
+              <Typography variant="h6" component="div" sx={{ margin: 2 }}>
+                {pkg.price}
+              </Typography>
+              <Button variant="contained" color="primary" sx={{ marginBottom: 2 }}>
+                Learn More
+              </Button>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
+
+export default Packages;
