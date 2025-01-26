f105607f6a155e586d5aec37abd843aceea2c708
Danny <danny.f.gomez@gmail.com>
2025-01-26 18:26:34 -0500

    update calendly url to redirect depending on chosen schedule

--- frontend/src/pages/schedulepage.jsx
+++ frontend/src/pages/schedulepage.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { InlineWidget } from 'react-calendly';
 import { Box } from '@mui/material';
+import { useParams } from 'react-router-dom';
 
 const VITE_CALENDLY_URL = import.meta.env.VITE_CALENDLY_URL;
 
 export default function SchedulePage() {
+  const { param } = useParams();
+  const calendlyUrl = `${VITE_CALENDLY_URL}/${param}`;
+
   return (
     <Box sx={{ paddingTop: '3.5rem', paddingX: '1.5rem', color: 'white' }}>
       <h1>Schedule</h1>
       <div className="App">
-        <InlineWidget url={VITE_CALENDLY_URL} />
+        <InlineWidget url={calendlyUrl} />
       </div>
     </Box>
   );
