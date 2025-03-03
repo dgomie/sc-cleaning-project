75c5000ac23da42c94b89cb4578c995f6709481f
Danny <danny.f.gomez@gmail.com>
2025-03-03 18:19:22 -0500

    add transparent logo

--- frontend/src/components/home/title.jsx
+++ frontend/src/components/home/title.jsx
@@ -1,25 +1,20 @@
-import { Box, useMediaQuery } from '@mui/material'
-import PhotoGrid from './photos'
-import Logo from '../../assets/images/DSC_Logo.jpg'
+import { Box, useMediaQuery } from '@mui/material';
+import PhotoGrid from './photos';
+import Logo from '../../assets/images/DSC_Logo_transparent_bg.png';
 
 export default function Title() {
-    const isSmallScreen = useMediaQuery('(max-width:768px)')
+  const isSmallScreen = useMediaQuery('(max-width:768px)');
 
-
-    return (
-        <>
-            <Box sx={{ fontSize: isSmallScreen ? '50px' : '64px' }}>
-              <img style={{maxHeight:'30vh'}} src={Logo} alt={'logo'}/>
-            </Box>
-            <br />
-            <Box sx={{ fontSize: isSmallScreen ? '36px' : '42px' }}>
-
-
-
-                {/* 999-999-9999 */}
-
-            </Box>
-            {/* {!isSmallScreen && (
+  return (
+    <>
+      <Box sx={{ fontSize: isSmallScreen ? '50px' : '64px' }}>
+        <img style={{ maxHeight: '30vh' }} src={Logo} alt={'logo'} />
+      </Box>
+      <br />
+      <Box sx={{ fontSize: isSmallScreen ? '36px' : '42px' }}>
+        {/* 999-999-9999 */}
+      </Box>
+      {/* {!isSmallScreen && (
                 <Box sx={{ marginTop: '70px', fontSize: '20px' }}>
                     <p>Before & After</p>
                     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
@@ -28,7 +23,6 @@ export default function Title() {
                     </Box>
                 </Box>
             )} */}
-
-        </>
-    )
+    </>
+  );
 }
